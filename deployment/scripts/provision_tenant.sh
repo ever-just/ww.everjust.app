eb3f23659676f0997f0d18c5a888f53f3e83f1bd
Everjust <company@everjust.org>
2026-06-07 14:04:03 -0500

    fix: track module data/ folder, scope gitignore to root, fix provision script

--- deployment/scripts/provision_tenant.sh
+++ deployment/scripts/provision_tenant.sh
@@ -16,12 +16,8 @@ docker exec "$ODOO_CONTAINER" odoo \
   --load-language=en_US \
   --stop-after-init --no-http
 
-echo "Setting admin login -> ${EMAIL}"
-docker exec "$ODOO_CONTAINER" psql -U everjust -d "$SUB" \
-  -c "UPDATE res_users SET login='${EMAIL}' WHERE id=2;"
-
-echo "Setting admin password"
-printf "env['res.users'].browse(2).write({'password': %r}); env.cr.commit()\n" "$PASS" \
+echo "Setting admin login + password -> ${EMAIL}"
+printf "env['res.users'].browse(2).write({'login': %r, 'password': %r}); env.cr.commit()\n" "$EMAIL" "$PASS" \
   | docker exec -i "$ODOO_CONTAINER" odoo shell -d "$SUB" --no-http
 
 echo "Done. Tenant live at https://${SUB}.everjust.app"

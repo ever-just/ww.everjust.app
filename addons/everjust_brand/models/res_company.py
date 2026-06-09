0c8de28174ab43c32a8a5936e47ffb44bdb73709
Everjust <company@everjust.org>
2026-06-09 10:33:11 -0500

    fix: remove res.company.favicon write (not in Odoo 19 Community)

--- addons/everjust_brand/models/res_company.py
+++ addons/everjust_brand/models/res_company.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import base64
 from odoo import api, models
-from odoo.tools import file_open
 
 
 class ResCompany(models.Model):
@@ -14,15 +12,6 @@ class ResCompany(models.Model):
         Called from data file on install so new tenant databases start
         fully branded with no upstream references in reports or emails.
         """
-        favicon_b64 = None
-        try:
-            with file_open('everjust_brand/static/img/favicon.ico', 'rb') as f:
-                favicon_b64 = base64.b64encode(f.read())
-        except Exception:
-            pass
-
         for company in self.search([]):
             if not company.report_footer or "odoo" in (company.report_footer or "").lower():
                 company.report_footer = "EVERJUST.APP"
-            if favicon_b64:
-                company.favicon = favicon_b64
